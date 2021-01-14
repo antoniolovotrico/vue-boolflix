851a9870919f7228195565908e4ef6eae9ae4dd4
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-01-14 18:39:54 +0100

    fixed bugs when flags and poster imgs are not found

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,12 @@ let app = new Vue ({
     data:{
         movies:null,
         search:"",
+        codeFlag:["ae", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "as", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bl", "bm", "bn", "bo", "br", "bs", "bt", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "ct", "cu", "cv", "cw", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "eh", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gg", "gh", "gi", "gl", "gm", "gn", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hn", "hr", "ht", "hu", "ic", "id", "ie", "il", "im", "in", "iq", "ir", "is", "it", "je", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mf", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sk", "sl", "sm", "sn", "so", "sr", "ss", "st", "sv", "sx", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "za", "zm", "zw"]
         
     },
     methods:{
         searchFunc:function(){
+          
             let apiOne = `https://api.themoviedb.org/3/search/movie?api_key=7eb147338689b7a3f2976a51bd64cbb5&query=${this.search}`;
 
             let apiTwo =`https://api.themoviedb.org/3/search/tv?api_key=7eb147338689b7a3f2976a51bd64cbb5&query=${this.search}`;
@@ -18,10 +20,10 @@ let app = new Vue ({
                 //console.log(response.data.results);
                 const responseapiOne = response[0].data.results;
                 const responseapiTwo = response[1].data.results;
-                console.log(responseapiOne);
-                console.log(responseapiTwo);
+                //console.log(responseapiOne);
+                //console.log(responseapiTwo);
                 this.movies = responseapiOne.concat(responseapiTwo);
-                console.log(this.movies); 
+                //console.log(this.movies); 
                 //console.log(this.search);
                 this.movies.forEach(element => {
                     let voteAver = Math.ceil(element.vote_average / 2);
@@ -56,11 +58,11 @@ let app = new Vue ({
                         element.original_language = "in";
                       } else if (element.original_language == "uk"){
                         element.original_language = "ua";
-                      }
-
+                      };
                     
                });
             }))
+            
         },
 
         
